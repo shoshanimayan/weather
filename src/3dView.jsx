8d7c0f82bdf20cf41c92a762a85d0c865d8936f7
shoshanimayan <shoshanimayan@gmail.com>
2025-01-03 17:58:37 -0800

    fixes

--- src/3dView.jsx
+++ src/3dView.jsx
@@ -36,36 +36,37 @@ const ModelView = ({data=null}) => {
             return ;
         }
         let path="";
+
         switch(modelPath)
         {
             case 800:
                 if(isDaytime(data)){
-                    path = '/icons/sun.glb'
+                    path = new URL('./icons/sun.glb', import.meta.url);
                 }
                 else
                 {
-                    path = '/icons/moon.glb'
+                    path = new URL('./icons/moon.glb', import.meta.url);
  
                 }
                 break;
             case 801: case 802: case 803: case 804:
-                path = '/icons/cloud.glb'
+                path = new URL('./icons/cloud.glb', import.meta.url);
                 break;
             case 600: case 601: case 602: case 611: case 612: case 613: case 615: case 616: case 620: case 621: case 622:
-                path = '/icons/cloud.glb'
+                path = new URL('./icons/cloud.glb', import.meta.url);
                 break ;
             case 300: case 301: case 302: case 310: case 311: case 312: case 313: case 314: case 321: case 500: case 501: case 502: case 503: case 504: case 511: case 520: case 521: case 522: case 531:
-                path = '/icons/rain.glb'
+                path = new URL('./icons/rain.glb', import.meta.url);
                  break;
             case 200: case 201: case 202: case 210: case 211: case 212: case 221: case 230: case 231: case 232:
-                path = '/icons/thunder.glb'
+                path = new URL('./icons/thunder.glb', import.meta.url);
                 break;
             default:
                 path="";
                 break;
         }
-
-        return  <Model path={path} position={[0, 0, 0]}/>
+        
+        return  <Model path={path+"/"} position={[0, 0, 0]}/>
 
 
     }
